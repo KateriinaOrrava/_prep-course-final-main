c1857e7f07907b18ba8e4d9578fcdb9964d97198
Katerina Orrava <98848233+KateRiinaO@users.noreply.github.com>
2022-11-23 13:30:55 +0200

    completed ts part of tic-tac-toe

--- 00-tic-tac-toe/src/Game.test.ts
+++ 00-tic-tac-toe/src/Game.test.ts
@@ -19,17 +19,18 @@ describe("Tic-Tac-Toe", () => {
     expect(game.getWinner()).toBe("-");
     expect(game.isTie()).toBe(false);
   });
-  //parbaudam vai uzklikšķinot pirmo reizi uz laukuma, tiek uzlikts X
+
+  //parbaudam vai uzklikšķinot otro reizi uz laukuma, tiek uzlikts O
   //***************************************************
-  it("should place X on click", () => {
+  it("should place X or O on click", () => {
     //pirms veikt katru testu izveido jaunu spēli
     //iedod spēlei sākotnejo stāvokli
     const game = new Game();
-
     game.onClick(0);
+    game.onClick(1);
     expect(game.getCells()).toEqual([
       "X",
-      "-",
+      "O",
       "-",
       "-",
       "-",
@@ -38,8 +39,85 @@ describe("Tic-Tac-Toe", () => {
       "-",
       "-",
     ]);
-    //expect(game.getTurn()).toBe("X");
+    expect(game.getTurn()).toBe("X");
     expect(game.getWinner()).toBe("-");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it("should place win if row field with player", () => {
+    //pirms veikt katru testu izveido jaunu spēli
+    //iedod spēlei sākotnejo stāvokli
+    const game = new Game();
+    game.onClick(0);
+    game.onClick(3);
+    game.onClick(1);
+    game.onClick(4);
+    game.onClick(2);
+
+    expect(game.getCells()).toEqual([
+      "X",
+      "X",
+      "X",
+      "O",
+      "O",
+      "-",
+      "-",
+      "-",
+      "-",
+    ]);
+
+    expect(game.getWinner()).toBe("X");
+    expect(game.isTie()).toBe(false);
+  });
+
+  it("should place win if row field with player", () => {
+    //pirms veikt katru testu izveido jaunu spēli
+    //iedod spēlei sākotnejo stāvokli
+    const game = new Game();
+    game.onClick(0);
+    game.onClick(1);
+    game.onClick(3);
+    game.onClick(4);
+    game.onClick(6);
+
+    expect(game.getCells()).toEqual([
+      "X",
+      "O",
+      "-",
+      "X",
+      "O",
+      "-",
+      "X",
+      "-",
+      "-",
+    ]);
+
+    expect(game.getWinner()).toBe("X");
+    expect(game.isTie()).toBe(false);
+  });
+  it("should place win if row field with player", () => {
+    //pirms veikt katru testu izveido jaunu spēli
+    //iedod spēlei sākotnejo stāvokli
+    const game = new Game();
+    game.onClick(0);
+    game.onClick(1);
+    game.onClick(4);
+    game.onClick(3);
+    game.onClick(8);
+
+    expect(game.getCells()).toEqual([
+      "X",
+      "O",
+      "-",
+      "O",
+      "X",
+      "-",
+      "-",
+      "-",
+      "X",
+    ]);
+
+    expect(game.getWinner()).toBe("X");
     expect(game.isTie()).toBe(false);
   });
 });
--- 00-tic-tac-toe/src/Game.ts
+++ 00-tic-tac-toe/src/Game.ts
@@ -1,4 +1,5 @@
-//te ir fefinēts tas, ka tipa vērtība var būt viena no zemāk norādītām un nekas cits
+/*te ir fefinēts tas, ka tipa vērtība var būt viena no zemāk norādītām 
+un nekas cits*/
 export type XO = "X" | "O" | "-";
 // šīs projekts ļauj ieskatīties tajā, kā notiek  "test driven development"
 /*Test-driven development (TDD) is a software development process 
@@ -7,32 +8,103 @@ relying on software requirements being converted to test cases before
  repeatedly testing the software against all test cases. This is as 
  opposed to software being developed first and test cases created later.*/
 export class Game {
-  //te definēts sākotnējs tukšs masīvs
+  /*te definēts sākotnējs tukšs masīvs - oriģinālā sākotnējā versija, 
+  kas turpmāk tiks izmainīta - starta stāvoklis*/
   cells: XO[] = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
-
+  /*te ir definēts, ka pirmā vērtība, kas tiks ielikta laukumiņā būs "X"*/
+  turn: XO = "X";
+  //funkcija, kas atgriež masīvu ar 9 vertībām
   getCells(): XO[] {
     return this.cells;
   }
-
+  //šī funkcija paņem turn vērtību
   getTurn(): XO {
-    return "X";
+    return this.turn;
   }
 
   getWinner(): XO {
+    //checks if the first row is filled by the same player
+    if (
+      this.getCells()[0] === this.getCells()[1] &&
+      this.getCells()[1] === this.getCells()[2]
+    ) {
+      return this.getCells()[0];
+    }
+    //for second row
+    if (
+      this.getCells()[3] === this.getCells()[4] &&
+      this.getCells()[4] === this.getCells()[5]
+    ) {
+      return this.getCells()[3];
+    }
+    //for third row
+    if (
+      this.getCells()[6] === this.getCells()[7] &&
+      this.getCells()[7] === this.getCells()[8]
+    ) {
+      return this.getCells()[6];
+    }
+
+    //for first column
+    if (
+      this.getCells()[0] === this.getCells()[3] &&
+      this.getCells()[3] === this.getCells()[6]
+    ) {
+      return this.getCells()[0];
+    }
+    //for second column
+    if (
+      this.getCells()[1] === this.getCells()[4] &&
+      this.getCells()[7] === this.getCells()[4]
+    ) {
+      return this.getCells()[1];
+    }
+    //for third column
+    if (
+      this.getCells()[2] === this.getCells()[5] &&
+      this.getCells()[5] === this.getCells()[8]
+    ) {
+      return this.getCells()[2];
+    }
+    //for first diag
+    if (
+      this.getCells()[0] === this.getCells()[4] &&
+      this.getCells()[4] === this.getCells()[8] &&
+      this.getCells()[0] !== "-"
+    ) {
+      return this.getCells()[0];
+    }
+    //for second diag
+    if (
+      this.getCells()[2] === this.getCells()[5] &&
+      this.getCells()[4] === this.getCells()[6] &&
+      this.getCells()[2] !== "-"
+    ) {
+      return this.getCells()[2];
+    }
+
     return "-";
   }
 
   isTie(): boolean {
-    return false;
+    const areAllCellsFilled: boolean = !this.getCells().includes("-");
+    const isNoWinner: boolean = this.getWinner() === "-";
+
+    return areAllCellsFilled && isNoWinner;
   }
 
   onClick(i: number): void {
-    this.getCells()[i] = "X";
+    //tas lauciņš, kura tiks ieklikšķināts iegūs vērtību - X
+    this.cells[i] = this.turn;
+    this.turn = this.turn === "X" ? "O" : "X";
+    //pēc "X" vienmēr sekos "O", tāpēc šeit tiek mainīta globālā TURN vertība
+
     /*pasaka kurā lauciņā ir ieklikšķināts 
     console.log(`cell ${i} clicked`);*/
   }
 
   restart(): void {
+    this.cells = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
     console.log("restart called");
   }
 }
